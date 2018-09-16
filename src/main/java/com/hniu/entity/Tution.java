00d4fcde77f9c00072bcbbefda390ba99bba5476
tiannoyi <1347492956@qq.com>
2018-09-16 11:01:26 +0800

    修改时间类型字段

--- src/main/java/com/hniu/entity/Tution.java
+++ src/main/java/com/hniu/entity/Tution.java
@@ -1,14 +1,16 @@
 package com.hniu.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 public class Tution {
     private Integer tutionId;
 
     private Integer curriculumId;
-
+    @JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
     private Date startTime;
-
+    @JsonFormat(locale="zh", timezone="GMT+8", pattern="yyyy-MM-dd HH:mm:ss")
     private Date endtime;
 
     private String schedules;
